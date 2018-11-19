d192cec243eb3b49c1befb33e2f8bc7350fc185b
madi-madi <ssss_-_2010@hotmail.com>
2018-11-19 19:53:10 +0200

    update footer 2

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -181,31 +181,10 @@
   <div class="control-sidebar-bg"></div>
 </div>
 
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<script>$.widget.bridge('uibutton', $.ui.button);</script>
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="bower_components/raphael/raphael.min.js"></script>
-<script src="bower_components/morris.js/morris.min.js"></script>
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<script src="bower_components/moment/min/moment.min.js"></script>
-<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<script src="dist/js/adminlte.min.js"></script>
-<script src="dist/js/pages/dashboard.js"></script>
-<script src="dist/js/demo.js"></script>
-11111
-<script src="{{ url('design/adminlte')}}/bower_components/jquery/dist/jquery.min.js"></script>
+
+<script src="{{url('design/adminlte')}}/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="{{url('design/adminlte')}}/bower_components/jquery-ui/jquery-ui.min.js"></script>
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
+<script>$.widget.bridge('uibutton', $.ui.button);</script>
 <script src="{{url('design/adminlte')}}/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="{{url('design/adminlte')}}/bower_components/raphael/raphael.min.js"></script>
 <script src="{{url('design/adminlte')}}/bower_components/morris.js/morris.min.js"></script>
@@ -222,6 +201,7 @@
 <script src="{{url('design/adminlte')}}/dist/js/adminlte.min.js"></script>
 <script src="{{url('design/adminlte')}}/dist/js/pages/dashboard.js"></script>
 <script src="{{url('design/adminlte')}}/dist/js/demo.js"></script>
+
   <script src="https://unpkg.com/native-toast@2.0.0/dist/native-toast.min.js"></script>
 
 
